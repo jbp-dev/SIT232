d10140eca11990692507b4c375810196dd282b56
JBP <s224113112@deakin.edu.au>
2026-07-08 20:07:41 +1000

    Add Week 1 practical tasks

---
+++ PracTask1_1/IfStatement.cs
@@ -0,0 +1,52 @@
+﻿namespace PracTask1_1
+{
+   internal class IfStatement
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Enter the number (as an integer): ");
+         int number = Convert.ToInt32(Console.ReadLine());
+         if (number == 1)
+         {
+            Console.WriteLine("ONE");
+         }
+         else if (number == 2)
+         {
+            Console.WriteLine("TWO");
+         }
+         else if (number == 3)
+         {
+            Console.WriteLine("THREE");
+         }
+         else if (number == 4)
+         {
+            Console.WriteLine("FOUR");
+         }
+         else if (number == 5)
+         {
+            Console.WriteLine("FIVE");
+         }
+         else if (number == 6)
+         {
+            Console.WriteLine("SIX");
+         }
+         else if (number == 7)
+         {
+            Console.WriteLine("SEVEN");
+         }
+         else if (number == 8)
+         {
+            Console.WriteLine("EIGHT");
+         }
+         else if (number == 9)
+         {
+            Console.WriteLine("NINE");
+         }
+         else if (number < 1 || number > 9)
+         {
+            Console.WriteLine("Please enter a number between 1 and 9: ");
+         }
+         
+      }
+   }
+}
