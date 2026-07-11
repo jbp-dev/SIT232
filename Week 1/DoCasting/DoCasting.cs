9f0103c560356889d99ce60d0f9e26106b74b383
JBP <s224113112@deakin.edu.au>
2026-07-11 21:44:15 +1000

    Rename Week 1 DoCasting project folder and update Week 1 code

---
+++ Week 1/DoCasting/DoCasting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
--- Week 1/SwitchStatement/SwitchStatement.cs
+++ Week 1/SwitchStatement/SwitchStatement.cs
@@ -13,6 +13,7 @@
             case 2: Console.WriteLine("Two"); break;
             case 3: Console.WriteLine("Three"); break;
             case 4: Console.WriteLine("Four"); break;
+          
             case 5: Console.WriteLine("Five"); break;
             case 6: Console.WriteLine("Six"); break;
             case 7: Console.WriteLine("Seven"); break;   
