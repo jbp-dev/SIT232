30815de9f4f687db03bd47eb4f43980d40585c27
JBP <s224113112@deakin.edu.au>
2026-07-24 10:58:21 +1000

    updated week 2 project

--- Week 2/GuessingNumber/GuessingNumber.cs
+++ Week 2/GuessingNumber/GuessingNumber.cs
@@ -1,5 +1,4 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
-
+﻿
 namespace GuessingNumber
 {
    public class GuessingNumber
@@ -8,20 +7,19 @@ namespace GuessingNumber
       {
          Console.WriteLine("Welcome to the Guessing Game!");
 
-         // Single player guessing game
-         //int guess;
+         // single player guessing game first attempt
+         //int guess = 0;
          //int number = 5; // The number to guess
 
          //while (true)
          //{
-         //   Console.Write("Guess a number between 1 and 10: ");
+         //   Console.Write("Guess the number between 1 and 10: ");
          //   if (int.TryParse(Console.ReadLine(), out guess) && guess >= 1 && guess <= 10)
          //   {
          //      break;
          //   }
          //   Console.WriteLine("Invalid input. Please enter a number between 1 and 10.");
          //}
-
          //while (guess != number)
          //{
          //   Console.Write("Wrong. Guess again: ");
@@ -33,47 +31,63 @@ namespace GuessingNumber
          //}
          //Console.WriteLine("You guessed the number! Well done!");
 
-         // Two players guessing game
-
-         int number;
+         // Single player guessing game with simple input validation
          int guess = 0;
+         int number = 5; // The number to guess
 
-         Console.WriteLine("Player 1: Enter a number between 1 and 10.");
-
-         // Keep asking Player 1 until they enter a valid number
-         while (true)
-         {
-            if (int.TryParse(Console.ReadLine(), out number) && number >= 1 && number <= 10)
-            {
-               break;
-            }
-
-            Console.WriteLine("Invalid input. Please enter a whole number between 1 and 10.");
-         }
-
-         // Clear screen so Player 2 cannot see the number
-         Console.Clear();
-
-         Console.WriteLine("Player 2: Guess the number between 1 and 10.");
-
-         // Keep asking Player 2 until they guess correctly
          while (guess != number)
          {
+            Console.Write("Guess a number between 1 and 10: ");
+
             if (!int.TryParse(Console.ReadLine(), out guess))
             {
-               Console.WriteLine("Invalid input. Please enter a whole number between 1 and 10.");
+               Console.WriteLine("Invalid input. Please enter a whole number.");
             }
             else if (guess < 1 || guess > 10)
             {
-               Console.WriteLine("Please enter a whole number between 1 and 10.");
+               Console.WriteLine("Please enter a number between 1 and 10.");
             }
-            else if (guess < number || guess > number)
+            else if (guess != number)
             {
                Console.WriteLine("Wrong. Guess again.");
             }
          }
-
          Console.WriteLine("You guessed the number! Well done!");
+
+         // Two players guessing game
+         //int number;
+         //int guess = 0;
+
+         //Console.WriteLine("Player 1: Enter a number between 1 and 10:");
+
+         //while (true) // Keep asking Player 1 until they enter a valid number
+         //{
+         //   if (int.TryParse(Console.ReadLine(), out number) && number >= 1 && number <= 10)
+         //   {
+         //      break;
+         //   }
+         //   Console.WriteLine("Invalid input. Please enter a whole number between 1 and 10.");
+         //}
+         //Console.Clear();  // Clear screen so Player 2 cannot see the number
+
+         //Console.WriteLine("Player 2: Guess Player 1's number between 1 and 10.");
+
+         //while (guess != number) // Keep asking Player 2 until they guess correctly
+         //{
+         //   if (!int.TryParse(Console.ReadLine(), out guess))
+         //   {
+         //      Console.WriteLine("Invalid input. Please enter a whole number between 1 and 10.");
+         //   }
+         //   else if (guess < 1 || guess > 10)
+         //   {
+         //      Console.WriteLine("Please enter a whole number between 1 and 10.");
+         //   }
+         //   else if (guess != number) // cleaner than (guess < number || guess > number)
+         //   {
+         //      Console.WriteLine("Wrong. Guess again.");
+         //   }
+         //}
+         //Console.WriteLine("You guessed the number! Well done!");
 
       }
    }
--- Week 2/Repetition/Repetition.cs
+++ Week 2/Repetition/Repetition.cs
@@ -14,14 +14,14 @@
          //The while loop is entry‑controlled: it checks its condition before each iteration and may not execute the body at all if the condition is false initially.
          //In contrast, the do…while loop is exit‑controlled: it always executes the body at least once before checking the condition, so one iteration will occur even when the condition is false at the start (for example, if upperbound were 101).
 
-         //for loop
-         //for (int number = 1; number <= upperbound; number++)
-         //{
-         //   sum += number;
-         //   Console.WriteLine("Current number: " + number + " the sum is " + sum);
-         //}
+         //for-loop
+         for (int number = 1; number <= upperbound; number++)
+         {
+            sum += number;
+            Console.WriteLine("Current number: " + number + " the sum is " + sum);
+         }
 
-         // while loop
+         //while-loop
          //int number = 1;
          //while (number <= upperbound)
          //{
@@ -30,17 +30,16 @@
          //   number++;
          //}
 
-         // do while loop
-         int number = 1;
-         do
-         {
-            sum += number;
-            Console.WriteLine("Current number: " + number + " the sum is " + sum);
-            number++;
-         } while (number <= upperbound);
+         // do-while-loop
+         //int number = 1;
+         //do
+         //{
+         //   sum += number;
+         //   Console.WriteLine("Current number: " + number + " the sum is " + sum);
+         //   number++;
+         //} while (number <= upperbound);
 
          average = (double)sum / upperbound;
-         
          Console.WriteLine("The sum is {0}", sum);
          Console.WriteLine("The average is {0}", average);
 
